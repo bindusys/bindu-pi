087325cc63bf8e350c29d84b816b10c70bf669a1
Pedro Morgan <pedromorgan@gmail.com>
2016-05-08 21:54:40 +0100

    gen backgroun

--- gen-background.sh
+++ gen-background.sh
@@ -6,3 +6,8 @@ convert -size 800x480 xc:white base.jpg
 
 # Create IP image
 convert base.jpg -pointsize 80 -fill blue -draw "text 0,150 'IPv4: $(ip -4 a s eth0 | grep -Eo 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | awk '{print $2}')'" -fill black -draw "text -0,250 'Hostname: $(uname -n)'" -pointsize 250  ip.jpg 
+
+sudo ln -s /etc/alternatives/desktop-background ./ip.jpg
+
+
+
